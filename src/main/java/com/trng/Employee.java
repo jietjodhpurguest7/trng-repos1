6aceaea4faf856c197cb2f25fa9e63f47f492d74
Guest07 Jodhpur <guest7@jietjodhpur.ac.in>
2023-10-07 22:28:22 +0530

    Added Employee class“

---
+++ src/main/java/com/trng/Employee.java
@@ -0,0 +1,40 @@
+package com.trng;
+
+public class Employee {
+    private String id;
+    private String name;
+    private String address;
+    private Double salary;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
